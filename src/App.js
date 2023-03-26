5b73f583429c0eef26b2850dbce318f4bc12796d
Abhiraj Trivedi <trivediabhiraj7@gmail.com>
2023-03-26 21:47:21 +0530

    commited all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Text from "./components/Text";
+import NotFound from "./components/404page";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 function App() {
+  let [mode, SetMode] = useState("light");
+  let toggleMode = () => {
+    if (mode === "light") {
+      document.body.style.backgroundColor = "#000000cf";
+      document.body.style.color = "white";
+      document.getElementById("MyNavBar").style.backgroundColor = "black";
+
+      document.getElementById("TitleNavBar").style.color = "white";
+
+      SetMode("dark");
+    } else {
+      SetMode("light");
+      document.body.style.backgroundColor = "#CDEEFF";
+      document.body.style.color = "black";
+      document.getElementById("MyNavBar").style.backgroundColor =
+        "rgb(0 170 255)";
+      document.getElementById("TitleNavBar").style.color = "blue";
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar mode={mode} logo="TextAbhi" toggleMode={toggleMode} />
+
+        <Routes>
+          <Route path="/" element={<Text mode={mode}></Text>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
